76d1f0166eee8c8fc629f3a0e5357795cb0825a0
Justina <justinakliu@gmail.com>
2022-12-13 17:25:21 -0500

    Removed CRA default components and styles. Added Cat and CatList components

---
+++ src/components/Cat.js
@@ -0,0 +1,10 @@
+const Cat = () => {
+  return (
+    <li>
+      <h3>Cat name:</h3>
+      <button>Add cat</button>
+    </li>
+  );
+};
+
+export default Cat;
---
+++ src/components/CatList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Cat from "./Cat";
+
+const CatList = () => {
+  return (
+    <ul>
+      <Cat />
+    </ul>
+  );
+};
+
+export default CatList;
